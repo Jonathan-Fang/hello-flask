36bbc35b28a0c9d294c18f151a44c95dfc607e03
Jonathan Fang <im.jonathan.fang@gmail.com>
2023-08-15 16:57:36 -0700

    attempted to make the button return text

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template
+import requests
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,3 +18,31 @@ def contact():
 @app.route("/youtube-api/")
 def youtube():
     return render_template("youtube-api.html")
+
+def execute_function():
+    # Insert function's logic here
+    # YouTube API.py
+    # test if I can do this first, and then try to make the function work
+    
+    # import requests
+
+    keyword = input("What video would you like to search for?\n") # keyword print success
+
+    API_response = requests.get ('https://youtube.googleapis.com/youtube/v3/search?q=' + keyword + '&key=AIzaSyDkxZGugtLQ_4-Ugz2iw-JxxvBJtssrKiM')
+    # response = requests.get ('https://youtube.googleapis.com/youtube/v3/search?q=barbie&key=AIzaSyDkxZGugtLQ_4-Ugz2iw-JxxvBJtssrKiM')
+
+    json = API_response.json() # json print success
+
+    video = json["items"][0]["id"]["videoId"] # video print success
+
+    video_url = ("https://youtube.com/watch?v=" + video)
+
+    print(video_url)
+    
+    return {"message": "Function executed successfully"}
+
+@app.route('/execute-function', methods=['Get'])
+def handle_execute_function():
+    # what is handle?
+    result = execute_function()
+    return jsonify(result)
