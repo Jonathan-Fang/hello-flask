307961e2f5a165c8f53af6581437a53b6c6e94f2
Jonathan Fang <im.jonathan.fang@gmail.com>
2023-08-15 23:40:08 -0700

    added 'main driver function' so that it would start development server when the app is running

--- app.py
+++ app.py
@@ -46,3 +46,9 @@ def handle_execute_function():
     # what is handle?
     result = execute_function()
     return jsonify(result)
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application on the local development server
+    app.run()
