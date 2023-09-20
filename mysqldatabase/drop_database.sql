7c5c62311885a7bfeaa9fd0a705c339c077358bc
Jonathan Fang <im.jonathan.fang@gmail.com>
2023-09-20 00:44:44 -0700

    added reference code for clearing the table with primary key

--- mysqldatabase/drop_database.sql
+++ mysqldatabase/drop_database.sql
@@ -1,3 +1,5 @@
 use flask_database;
+DELETE FROM flask_table WHERE flask_table.PrimaryID = 1;
+DELETE FROM flask_table WHERE (flask_table.fname='fnameblah' and flask_table.PrimaryID > 0);
 DROP table flask_table;
 DROP Database flask_database;
